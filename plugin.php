d6a97738081dfe04e9f4a7ea6f44c24beb64ad8a
Fabian Schmid <fs@studer-raimann.ch>
2016-05-17 11:00:32 +0200

    plugin version

--- plugin.php
+++ plugin.php
@@ -1,8 +1,7 @@
 <?php
 $id = 'xpho';
-$version = '2.2.3';
+$version = '2.2.4';
 $ilias_min_version = '5.0.0';
 $ilias_max_version = '5.1.999';
 $responsible = 'Gabriel Comte, Fabian Schmid, Martin Studer';
 $responsible_mail = 'support@studer-raimann.ch';
-?>
