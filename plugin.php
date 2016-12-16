4b52ffab49a57b3e2229070e2323478368058250
theodortruffer <tt@studer-raimann.ch>
2016-12-16 09:57:08 +0100

    updated plugin version

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 $id = 'xpho';
-$version = '2.2.5';
+$version = '2.2.6';
 $ilias_min_version = '5.0.0';
 $ilias_max_version = '5.2.999';
 $responsible = 'Gabriel Comte, Fabian Schmid, Martin Studer';
