7b2722cb1eb90018954c8472e266f5ad7ca84277
Florian <fw@studer-raimann.ch>
2018-01-11 09:53:24 +0100

    Remove old ILIAS 5.0/5.1 support

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'xpho';
 $version = '2.3.8';
-$ilias_min_version = '5.0.0';
+$ilias_min_version = '5.2.0';
 $ilias_max_version = '5.3.999';
 $responsible = 'Gabriel Comte, Fabian Schmid, Martin Studer';
 $responsible_mail = 'support@studer-raimann.ch';
