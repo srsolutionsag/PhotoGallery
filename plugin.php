a25abd8eaada8ab1f6be26551d5a6eae20435078
Benjamin Seglias <bs@studer-raimann.ch>
2018-04-18 16:55:37 +0200

    updaed plugin version

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 $id = 'xpho';
-$version = '2.3.9';
+$version = '2.4.9';
 $ilias_min_version = '5.2.0';
 $ilias_max_version = '5.3.999';
 $responsible = 'Gabriel Comte, Fabian Schmid, Martin Studer';
