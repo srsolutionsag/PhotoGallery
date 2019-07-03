eb6bee1d5379ed8e86bc5922a07b56ff8a2a2eba
Theodor Truffer <tt@studer-raimann.ch>
2019-07-03 12:15:58 +0200

    v2.5.1

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 $id = 'xpho';
-$version = '2.5.0';
+$version = '2.5.1';
 $ilias_min_version = '5.3.0';
 $ilias_max_version = '5.4.999';
 $responsible = 'Gabriel Comte, Fabian Schmid, Martin Studer';
