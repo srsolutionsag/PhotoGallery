3050694da72265ec21f1d780c02baac6f8fc3da5
Fabian Schmid <fs@studer-raimann.ch>
2020-08-07 08:35:34 +0200

    plugin version

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 $id                = 'xpho';
-$version           = '2.5.1';
+$version           = '2.6.0';
 $ilias_min_version = '5.3.0';
 $ilias_max_version = '5.4.999';
 $responsible       = 'studer + raimann ag';
