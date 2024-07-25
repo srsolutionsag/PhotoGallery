9d517fbf482eb8ec3286394c6c119e19afe73cf3
Lukas Zehnder <lukas@sr.solutions>
2024-07-25 06:18:10 +0200

    [FIX] Fixed action calls of album picture management (IRSS functionality still WIP)

--- classes/Picture/class.srObjPictureGUI.php
+++ classes/Picture/class.srObjPictureGUI.php
@@ -1,5 +1,8 @@
 <?php
 
+use ILIAS\HTTP\Services;
+use ILIAS\Refinery\Factory AS Refinery;
+
 /**
  * GUI-Class srObjPictureGUI
  * @author            Fabian Schmid <fs@studer-raimann.ch>
@@ -16,34 +19,17 @@ class srObjPictureGUI
     public const CMD_REDIRECT_TO_ALBUM_LIST_PICTURES = 'redirectToAlbumListPictures';
     public const CMD_REDIRECT_TO_ALBUM_MANAGE_PICTURES = 'redirectToAlbumManagePictures';
     public const CMD_SEND_FILE = 'sendFile';
-    /**
-     * @var ilAccessHandler
-     */
-    protected $access;
-    /**
-     * @var ilTabsGUI
-     */
-    protected $tabs_gui;
-    /**
-     * @var ilPropertyFormGUI
-     */
-    protected $form;
-    /**
-     * @var ilToolbarGUI
-     */
-    protected $toolbar;
-    /**
-     * @var ilCtrl
-     */
-    protected $ctrl;
-    /**
-     * @var ilTemplate
-     */
-    protected $tpl;
-    /**
-     * @var \ActiveRecord|null
-     */
-    public $obj_picture;
+
+    protected ilAccessHandler $access;
+    protected ilTabsGUI $tabs_gui;
+    protected ilPropertyFormGUI $form;
+    protected ilToolbarGUI $toolbar;
+    protected ilCtrl $ctrl;
+    private Services $http;
+    private ilLanguage $lng;
+    private Refinery $refinery;
+    protected ilGlobalTemplateInterface $tpl;
+    public \ActiveRecord|null $obj_picture;
     public ILIAS\DI\UIServices $ui;
 
     /**
@@ -55,7 +41,10 @@ class srObjPictureGUI
         $this->tpl = $DIC->ui()->mainTemplate();
         $this->access = $DIC->access();
         $this->ctrl = $DIC->ctrl();
+        $this->http = $DIC->http();
+        $this->lng = $DIC->language();
         $this->parent = $parent_gui;
+        $this->refinery = $DIC->refinery();
         $this->toolbar = $DIC->toolbar();
         $this->tabs_gui = $DIC->tabs();
         $this->obj_picture = srObjPicture::find($_GET['picture_id']);
@@ -128,14 +117,17 @@ class srObjPictureGUI
 
     public function edit(): void
     {
-        if (!$this->access->checkAccess('write', '', $this->parent->getRefId())) {
-            $this->ui->mainTemplate()->setOnScreenMessage("failure", $this->pl->txt('permission_denied'), true);
-            $this->ctrl->redirect($this, '');
-        } else {
-            $form = new srObjPictureFormGUI($this, $this->obj_picture);
-            $form->fillForm();
-            $this->tpl->setContent($form->getHTML());
-        }
+        $picture_ids = $this->retrievePictureIDs();
+        $picture_id = $picture_ids[0];
+        $this->ctrl->setParameterByClass(srObjPictureFormGUI::class, 'picture_id', $picture_id);
+        $this->ctrl->setParameter($this, 'picture_id', $picture_id);
+        /**
+         * @var $picture srObjPicture
+         */
+        $picture = srObjPicture::find($picture_id);
+        $form_gui = new srObjPictureFormGUI($this, $picture);
+        $form_gui->fillForm();
+        $this->tpl->setContent($form_gui->getHTML());
     }
 
     public function update(): void
@@ -161,94 +153,72 @@ class srObjPictureGUI
 
     public function confirmDelete(): void
     {
-        $arr_picture_ids = [];
-        if (!$this->access->checkAccess('write', '', $this->parent->getRefId())) {
-            $this->ui->mainTemplate()->setOnScreenMessage("failure", $this->pl->txt('permission_denied'), true);
-            $this->ctrl->redirect($this, self::CMD_REDIRECT_TO_ALBUM_MANAGE_PICTURES);
-        } else {
-            if ((!isset($_POST['picture_ids']) || !(is_countable($_POST['picture_ids']) ? count(
-                $_POST['picture_ids']
-            ) : 0)) && !$_GET['picture_id']) {
-                $this->ui->mainTemplate()->setOnScreenMessage("failure", $this->pl->txt('no_checkbox'), true);
-                $this->ctrl->redirect($this, self::CMD_REDIRECT_TO_ALBUM_MANAGE_PICTURES);
+        $picture_ids = $this->retrievePictureIDs();
+        $this->ctrl->setParameterByClass(srObjPictureGUI::class, 'picture_ids', implode(',', $picture_ids));
+        $delete_action = $this->ctrl->getLinkTarget($this, 'delete');
+        $this->ctrl->clearParameterByClass(srObjPictureGUI::class, 'picture_ids');
+        $items = [];
+        foreach ($picture_ids as $picture_id) {
+            /**
+             * @var $picture srObjPicture
+             */
+            $picture = srObjPicture::find($picture_id);
+            if ($picture === null) {
+                continue;
             }
-            if (isset($_POST['picture_ids']) && (is_countable($_POST['picture_ids']) ? count(
-                $_POST['picture_ids']
-            ) : 0)) {
-                $arr_picture_ids = $_POST['picture_ids'];
-            } else {
-                $arr_picture_ids[] = $_GET['picture_id'];
-            }
-            $c_gui = new ilConfirmationGUI();
-            $c_gui->setFormAction($this->ctrl->getFormAction($this, atTableGUI::CMD_DELETE));
-            $c_gui->setHeaderText($this->pl->txt('delete_pic'));
-            $c_gui->setCancel($this->pl->txt('cancel'), srObjPictureGUI::CMD_REDIRECT_TO_ALBUM_MANAGE_PICTURES);
-            $c_gui->setConfirm($this->pl->txt('delete'), atTableGUI::CMD_DELETE);
-            // add items to delete
-            foreach ($arr_picture_ids as $picture_id) {
-                /**
-                 * @var $srObjPicture srObjPicture
-                 */
-                $srObjPicture = srObjPicture::find($picture_id);
-                $file_icon = ilObject::_getIcon($srObjPicture->getId(), "small", "file");
-                $c_gui->addItem('picture_ids[]', $picture_id, $srObjPicture->getTitle(), $file_icon);
-            }
-            $this->tpl->setContent($c_gui->getHTML());
+            $picture_title = $picture->getTitle();
+            $this->ctrl->setParameterByClass(srObjPictureGUI::class, 'picture_id', $picture_id);
+            $this->ctrl->setParameterByClass(srObjPictureGUI::class, 'picture_type', srObjPicture::TITLE_PREVIEW);
+            $src_preview = $this->ctrl->getLinkTargetByClass(srObjPictureGUI::class, srObjPictureGUI::CMD_SEND_FILE);
+            $image = $this->ui->factory()->image()->standard($src_preview, $picture_title);
+            $items[] = $this->ui->factory()->modal()->interruptiveItem()->standard(
+                $picture_id,
+                $picture_title,
+                $image
+            );
         }
+        $interruptive_modal = $this->ui->factory()->modal()->interruptive(
+            $this->lng->txt('delete'),
+            $this->pl->txt('delete_picture'),
+            $delete_action
+        )->withAffectedItems($items);
+        echo($this->ui->renderer()->renderAsync([$interruptive_modal]));
+        exit();
     }
 
     public function delete(): void
     {
-        $album_id = null;
-        if (!$this->access->checkAccess('write', '', $this->parent->getRefId())) {
-            $this->ui->mainTemplate()->setOnScreenMessage("failure", $this->pl->txt('permission_denied'), true);
-            $this->ctrl->redirect($this, self::CMD_REDIRECT_TO_ALBUM_MANAGE_PICTURES);
-        } else {
-            if ((is_countable($_POST['picture_ids']) ? count($_POST['picture_ids']) : 0) > 0) {
-                foreach ($_POST['picture_ids'] as $pic_id) {
-                    /**
-                     * @var $srObjPicture srObjPicture
-                     */
-                    $srObjPicture = srObjPicture::find($pic_id);
-                    //saveAlbumID before Deletion
-                    $album_id = $srObjPicture->getAlbumId();
-                    //if the current picture serves as the album's preview image remove the preview before deletion
-                    $srObjAlbum = srObjAlbum::find($album_id);
-                    if ($srObjPicture->getId() == $srObjAlbum->getPreviewId()) {
-                        $srObjAlbum->setPreviewId(0);
-                        $srObjAlbum->update();
-                    }
-
-                    $srObjPicture->delete();
+        $picture_ids = array_map('intval', $this->http->request()->getParsedBody()['interruptive_items']);
+        if ((is_countable($picture_ids) ? count($picture_ids) : 0) > 0) {
+            // delete all selected items
+            foreach ($picture_ids as $picture_id) {
+                /**
+                 * @var $picture srObjPicture
+                 */
+                $picture = srObjPicture::find($picture_id);
+                if($picture === null) {
+                    continue;
                 }
-                $this->ui->mainTemplate()->setOnScreenMessage("success", $this->pl->txt('msg_removed_pic'), true);
-            } else {
-                $this->ui->mainTemplate()->setOnScreenMessage("failure", $this->pl->txt('no_checkbox'), true);
+                //if the current picture serves as the album's preview image remove the preview before deletion
+                $album_id = $picture->getAlbumId();
+                $album = srObjAlbum::find($album_id);
+                if ($album !== null && ((int) $picture->getId() === $album->getPreviewId())) {
+                    $album->setPreviewId(0);
+                    $album->update();
+                }
+
+                $picture->delete();
             }
-            $this->ctrl->setParameterByClass(srObjAlbumGUI::class, 'album_id', $album_id);
-            $this->ctrl->redirectByClass(srObjAlbumGUI::class, srObjAlbumGUI::CMD_MANAGE_PICTURES);
+            $this->ui->mainTemplate()->setOnScreenMessage("success", $this->pl->txt('msg_removed_album'), true);
+        } else {
+            $this->ui->mainTemplate()->setOnScreenMessage("failure", $this->pl->txt('no_checkbox'), true);
         }
+        $this->ctrl->redirectByClass(srObjAlbumGUI::class, srObjAlbumGUI::CMD_MANAGE_PICTURES);
     }
 
     public function download(): void
     {
-        $arr_picture_ids = [];
-        if (!$this->access->checkAccess('read', '', $this->parent->getRefId())) {
-            $this->ui->mainTemplate()->setOnScreenMessage("failure", $this->pl->txt('permission_denied'), true);
-            $this->ctrl->redirect($this, '');
-        } else {
-            if (!empty($_REQUEST['picture_id'])) {
-                $arr_picture_ids[] = $_REQUEST['picture_id'];
-            } elseif (!empty($_REQUEST['picture_ids'])) {
-                $arr_picture_ids = $_POST['picture_ids'];
-            } else {
-                $this->ui->mainTemplate()->setOnScreenMessage("failure", $this->pl->txt('no_checkbox'), true);
-                $this->ctrl->setParameterByClass(srObjAlbumGUI::class, 'picutre_id', null);
-                $this->ctrl->setParameterByClass(srObjAlbumGUI::class, 'album_id', $_GET['album_id']);
-                $this->ctrl->redirectByClass(srObjAlbumGUI::class, srObjAlbumGUI::CMD_MANAGE_PICTURES);
-            }
-            ilObjPhotoGalleryGUI::executeDownload($arr_picture_ids);
-        }
+        ilObjPhotoGalleryGUI::executeDownload($this->retrievePictureIDs());
     }
 
     protected function sendFile(): void
@@ -263,5 +233,63 @@ class srObjPictureGUI
         $srObjPicture = srObjPicture::find($_GET['picture_id']);
         $path_to_file = $srObjPicture->getSrc($_GET['picture_type']);
         ilFileDelivery::deliverFileInline($path_to_file, $srObjPicture->getTitle() . '.' . $srObjPicture->getSuffix());
+    }
+
+
+    protected function retrievePictureIDs(): array
+    {
+        $album_id = $this->retrieveAlbumID();
+        if (!$this->access->checkAccess('read', '', $this->parent->getRefId())) {
+            $this->ui->mainTemplate()->setOnScreenMessage("failure", $this->pl->txt('permission_denied'), true);
+            $this->ctrl->setParameterByClass(srObjAlbumGUI::class, 'album_id', $album_id);
+            $this->ctrl->redirectByClass(srObjAlbumGUI::class, srObjAlbumGUI::CMD_MANAGE_PICTURES);
+        }
+        if (!$this->http->wrapper()->query()->has('album_picture_ids')) {
+            $this->ui->mainTemplate()->setOnScreenMessage("failure", $this->pl->txt('no_picture_ids'), true);
+            $this->ctrl->setParameterByClass(srObjAlbumGUI::class, 'album_id', $album_id);
+            $this->ctrl->redirectByClass(srObjAlbumGUI::class, srObjAlbumGUI::CMD_MANAGE_PICTURES);
+        }
+        $to_int = $this->refinery->kindlyTo()->string();
+        $to_str_array = $this->refinery->kindlyTo()->listOf($to_int);
+        $picture_ids = $this->http->wrapper()->query()->retrieve('album_picture_ids', $to_str_array);
+        if (empty($picture_ids)) {
+            $this->ui->mainTemplate()->setOnScreenMessage("failure", $this->pl->txt('no_picture_ids'), true);
+            $this->ctrl->setParameterByClass(srObjAlbumGUI::class, 'album_id', $album_id);
+            $this->ctrl->redirectByClass(srObjAlbumGUI::class, srObjAlbumGUI::CMD_MANAGE_PICTURES);
+        }
+        $album = srObjAlbum::find($album_id);
+        if ($album === null) {
+            $this->ui->mainTemplate()->setOnScreenMessage("failure", $this->pl->txt('no_album'), true);
+            $this->ctrl->setParameterByClass(srObjAlbumGUI::class, 'album_id', $album_id);
+            $this->ctrl->redirectByClass(srObjAlbumGUI::class, srObjAlbumGUI::CMD_MANAGE_PICTURES);
+        }
+
+        // handle ALL_OBJECTS special case
+        if($picture_ids[0] === 'ALL_OBJECTS') {
+            $picture_ids = [];
+            $pictures = $album->getPictureObjects();
+            foreach ($pictures as $picture) {
+                $picture_ids[] = $picture->getId();
+            }
+        } else {
+            $picture_ids = array_map('intval', $picture_ids);
+        }
+        return $picture_ids;
+    }
+
+    protected function retrieveAlbumID(): int
+    {
+        if (!$this->http->wrapper()->query()->has('album_id')) {
+            $this->ui->mainTemplate()->setOnScreenMessage("failure", $this->pl->txt('no_album_id'), true);
+            $this->ctrl->redirect($this, self::CMD_REDIRECT_TO_ALBUM_MANAGE_PICTURES);
+        }
+        $to_int = $this->refinery->kindlyTo()->string();
+        $album_id = $this->http->wrapper()->query()->retrieve('album_id', $to_int);
+        if (empty($album_id)) {
+            $this->ui->mainTemplate()->setOnScreenMessage("failure", $this->pl->txt('no_album_id'), true);
+            $this->ctrl->redirect($this, self::CMD_REDIRECT_TO_ALBUM_MANAGE_PICTURES);
+        }
+
+        return $album_id;
     }
 }
